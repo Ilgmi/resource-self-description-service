3d0ea34047fc6f0294389011053defa00a24b555
Ilgmi <michael.ilg@mailbox.org>
2025-05-15 11:26:12 +0200

    refactor tests

--- templating/src/test/java/org/eclipse/slm/self_description_service/templating/method/TemplateRendererTests.java
+++ templating/src/test/java/org/eclipse/slm/self_description_service/templating/method/TemplateRendererTests.java
@@ -4,10 +4,7 @@ import org.eclipse.slm.self_description_service.templating.TemplateRenderer;
 import org.eclipse.slm.self_description_service.templating.method.utils.PathHelper;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -27,12 +24,6 @@ class TemplateTest {
 
     public GetResult getResult;
 
-    public TemplateTest(HashMap<String, Object> data, String testTemplate, GetResult expectedResult) {
-        this.data = data;
-        this.testTemplate = testTemplate;
-        this.getResult = expectedResult;
-    }
-
     public TemplateTest(String testTemplate, GetResult expectedResult) {
         this.testTemplate = testTemplate;
         this.getResult = expectedResult;
@@ -42,13 +33,6 @@ class TemplateTest {
 
 public class TemplateRendererTests {
 
-
-    private String getPathForFile(String fileName) throws URISyntaxException {
-        URL res = getClass().getClassLoader().getResource(fileName);
-        File file = Paths.get(res.toURI()).toFile();
-        String absolutePath = file.getAbsolutePath().replace("\\", "\\\\");
-        return absolutePath;
-    }
 
     @Test
     public void RenderTest() throws URISyntaxException {
@@ -67,47 +51,33 @@ public class TemplateRendererTests {
                 ),
                 new TemplateTest(
                         "<#assign x = \"something\">${x?indexOf(\"met\")}",
-                        () -> {
-                            return "2";
-                        }
+                        () -> "2"
                 ),
                 new TemplateTest(
                         "${\"hello\"?capitalize}",
-                        () -> {
-                            return "Hello";
-                        }
+                        () -> "Hello"
                 ),
                 new TemplateTest(
                         String.format("${JsonFileValue(\"%s\", \"%s\")?string[\"0.###\"]}", "$.['price range'].cheap",
                                 PathHelper.getPathForFile(this, "json/simple_file.json", osIsWindows)),
-                        () -> {
-                            return "10.123";
-                        }
+                        () -> "10.123"
                 ),
                 new TemplateTest(
                         String.format("${YamlFileValue(\"%s\", \"%s\")?string[\"0.0\"]}", "$.['price range'].cheap",
                                 PathHelper.getPathForFile(this, "yaml/simple_file.yaml", osIsWindows)),
-                        () -> {
-                            return "10.1";
-                        }
+                        () -> "10.1"
                 ),
                 new TemplateTest(
                         String.format("${CV(\"%s\", \"%s\", \"%s\")?string[\"0.0\"]}", "echo {'price range':{'cheap':10}}", "j", "$.['price range'].cheap"),
-                        () -> {
-                            return "10.0";
-                        }
+                        () -> "10.0"
                 ),
                 new TemplateTest(
                         String.format("${CV('%s', \"%s\", \"%s\")}", "echo \"<root><element>Some data</element></root>\"", "x", "/root/element"),
-                        () -> {
-                            return "Some data";
-                        }
+                        () -> "Some data"
                 ),
                 new TemplateTest(
                         String.format("${CV(\"%s\", \"%s\", \"%s\")}", "echo some element", "r", "element"),
-                        () -> {
-                            return "element";
-                        }
+                        () -> "element"
                 )
         );
 
